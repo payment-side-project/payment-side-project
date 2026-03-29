9a31a0fdd60331dc7049f3102c58a32788a081df
godchm <choehyeongmin7@gmail.com>
2026-03-29 21:25:39 +0900

    feat: Initial commit

---
+++ src/main/java/com/paymentsideproject/PaymentSideProjectApplication.java
@@ -0,0 +1,13 @@
+package com.paymentsideproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSideProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentSideProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paymentsideproject/PaymentSideProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentsideproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSideProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
