31600acb3324a9317f20d8c0a3ad0dc7ef34a66e
godchm <choehyeongmin7@gmail.com>
2026-04-05 23:50:25 +0900

    feat: springsecurity, jwtutil

---
+++ src/main/java/com/paymentsideproject/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.paymentsideproject.config;
+
+
+import com.paymentsideproject.security.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers(LOGIN_PATH).permitAll()
+//                        .requestMatchers(ADMIN_PATH).hasAuthority("ADMIN")
+//                        .requestMatchers(NORMAL_PATH).hasAnyAuthority("NORMAL", "ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+}
--- src/main/java/com/paymentsideproject/domain/customer/entity/Customer.java
+++ src/main/java/com/paymentsideproject/domain/customer/entity/Customer.java
@@ -1,7 +1,9 @@
 package com.paymentsideproject.domain.customer.entity;
 
 
+import com.paymentsideproject.domain.customer.enums.Rank;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -14,5 +16,20 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 고객 이름
+    @NotBlank(message = "이름은 필수 입력값입니다.")
+    private String name;
+
+    // 고객 이메일
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    private String email;
+
+
+    // 고객 비밀번호
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Rank rank;
 
 }
